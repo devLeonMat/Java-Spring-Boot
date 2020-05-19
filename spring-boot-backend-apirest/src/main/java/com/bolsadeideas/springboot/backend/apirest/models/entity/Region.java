77fa8448228358ba11a063b9633005174a34041c
leon matias <leonmatias1991@gmail.com>
2020-05-18 20:06:26 -0500

    add new porject

---
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Region.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "regiones")
+public class Region implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	private static final long serialVersionUID = 1L;
+
+}
